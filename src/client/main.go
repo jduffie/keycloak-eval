067a1a5de35824778e8b7b049a2e655fbfda3ad8
John Duffie <john.duffie@xyleminc.com>
2020-07-15 21:05:33 -0400

    Authorization Code Grant: A thru C

--- src/client/main.go
+++ src/client/main.go
@@ -39,33 +39,7 @@ func main()  {
 	http.ListenAndServe(":8080", nil)
 }
 
-func logout(writer http.ResponseWriter, request *http.Request) {
-	log.Printf("logout: Request queries: %v", request.URL.Query())
 
-	qs:= url.Values{}
-	qs.Add("redirect_uri", config.afterLogoutRedirect)
-	logoutURL, err := url.Parse(config.logout)
-	if err != nil {
-		log.Print(err)
-		return
-	}
-	logoutURL.RawQuery = qs.Encode()
-	appVar = AppVar{}
-	http.Redirect(writer, request, logoutURL.String(), http.StatusFound)
-	log.Printf("logout: done")
-}
-
-// AS will send us back here after login so we can collect the creds
-//    and then redirect home
-func authCodeRedirect(writer http.ResponseWriter, request *http.Request) {
-	log.Printf("authCodeRedirect: Request queries: %v", request.URL.Query())
-	appVar.AuthCode = request.URL.Query().Get("code")
-	appVar.SessionState = request.URL.Query().Get("session_state")
-	request.URL.RawQuery = ""
-	log.Printf("Request queries: %+v", appVar)
-	http.Redirect(writer, request, "http://localhost:8080", http.StatusFound)
-	log.Printf("authCodeRedirect: done")
-}
 
 func home(writer http.ResponseWriter, request *http.Request) {
 	log.Printf("home: Request queries: %v", request.URL.Query())
@@ -73,6 +47,14 @@ func home(writer http.ResponseWriter, request *http.Request) {
 	log.Printf("home: done")
 }
 
+//(A)  The client initiates the flow by directing the resource owner's
+//user-agent to the authorization endpoint.  The client includes
+//its client identifier, requested scope, local state, and a
+//redirection URI to which the authorization server will send the
+//user-agent back once access is granted (or denied).
+//(B)  The authorization server authenticates the resource owner (via
+//the user-agent) and establishes whether the resource owner
+//grants or denies the client's access request.
 func login(writer http.ResponseWriter, request *http.Request) {
 	log.Printf("login: Request queries: %v", request.URL.Query())
 	req, err := http.NewRequest("GET", config.authURL,nil)
@@ -90,3 +72,40 @@ func login(writer http.ResponseWriter, request *http.Request) {
 	log.Printf("login: done")
 }
 
+//(C)  Assuming the resource owner grants access, the authorization
+//server redirects the user-agent back to the client using the
+//redirection URI provided earlier (in the request or during
+//client registration).  The redirection URI includes an
+//authorization code and any local state provided by the client
+//earlier.
+//(D)  The client requests an access token from the authorization
+//server's token endpoint by including the authorization code
+//received in the previous step.  When making the request, the
+//client authenticates with the authorization server.  The client
+//includes the redirection URI used to obtain the authorization
+//code for verification.
+func authCodeRedirect(writer http.ResponseWriter, request *http.Request) {
+	log.Printf("authCodeRedirect: Request queries: %v", request.URL.Query())
+	appVar.AuthCode = request.URL.Query().Get("code")
+	appVar.SessionState = request.URL.Query().Get("session_state")
+	request.URL.RawQuery = ""
+	log.Printf("Request queries: %+v", appVar)
+	http.Redirect(writer, request, "http://localhost:8080", http.StatusFound)
+	log.Printf("authCodeRedirect: done")
+}
+
+func logout(writer http.ResponseWriter, request *http.Request) {
+	log.Printf("logout: Request queries: %v", request.URL.Query())
+
+	qs:= url.Values{}
+	qs.Add("redirect_uri", config.afterLogoutRedirect)
+	logoutURL, err := url.Parse(config.logout)
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	logoutURL.RawQuery = qs.Encode()
+	appVar = AppVar{}
+	http.Redirect(writer, request, logoutURL.String(), http.StatusFound)
+	log.Printf("logout: done")
+}
