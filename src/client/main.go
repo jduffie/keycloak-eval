793b705a8abf332fc85b8d8ce169f251bfe7911c
John Duffie <john.duffie@xyleminc.com>
2020-07-17 15:14:03 -0400

    authorization code flow: full

--- src/client/main.go
+++ src/client/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"html/template"
+	"io/ioutil"
+	"learn.oauth.client/model"
 	"log"
 	"net/http"
 	"net/url"
+	"strings"
 )
 
 var config = struct {
@@ -14,19 +18,24 @@ var config = struct {
 	logout                   string
 	afterLogoutRedirect      string
 	clientId                 string
+	tokenEndpointURL         string
+	clientSecret             string
 }{
 	authURL:                  "http://10.100.196.60:8080/auth/realms/learningApp/protocol/openid-connect/auth",
+	tokenEndpointURL:         "http://10.100.196.60:8080/auth/realms/learningApp/protocol/openid-connect/token",
 	afterAuthCodeRedirectURL: "http://localhost:8080/afterAuthCodeRedirectURL",
 	logout:                   "http://10.100.196.60:8080/auth/realms/learningApp/protocol/openid-connect/logout",
 	afterLogoutRedirect:      "http://localhost:8080",
 	clientId:                 "billingApp",
+	clientSecret:              "6538377f-b199-4e90-85bf-ca0d1f7911bd",
 }
 
 var t = template.Must(template.ParseFiles("template/index.html"))
 
 type AppVar struct {
-	AuthCode         string
-	SessionState     string
+	AuthCode     string
+	SessionState string
+	AccessToken  string
 }
 var appVar = AppVar{}
 
@@ -37,10 +46,15 @@ func main()  {
 
 	http.HandleFunc("/", home)
 	http.HandleFunc("/login", login)
+	// temp for now for demo purpose - will later be done via client, itself
+	http.HandleFunc("/exchangeToken", exchangeToken)
+
 	http.HandleFunc("/logout", logout)
 	http.HandleFunc("/afterAuthCodeRedirectURL", authCodeRedirect)
 	http.ListenAndServe(":8080", nil)
 }
+
+
 
 
 
@@ -49,6 +63,7 @@ func home(writer http.ResponseWriter, request *http.Request) {
 	t.Execute(writer, appVar)
 	log.Printf("home: done")
 }
+
 
 //(A)  The client initiates the flow by directing the resource owner's
 //user-agent to the authorization endpoint.  The client includes
@@ -95,6 +110,53 @@ func authCodeRedirect(writer http.ResponseWriter, request *http.Request) {
 	log.Printf("Request queries: %+v", appVar)
 	http.Redirect(writer, request, "http://localhost:8080", http.StatusFound)
 	log.Printf("authCodeRedirect: done")
+}
+
+
+func exchangeToken(writer http.ResponseWriter, request *http.Request) {
+	log.Printf("exchangeToken: Request queries: %v", request.URL.Query())
+	// Request
+	form:= url.Values{}
+	form.Add("state", "123")
+	form.Add("grant_type", "authorization_code")
+	form.Add("code", appVar.AuthCode)
+	form.Add("redirect_uri", config.afterAuthCodeRedirectURL)
+	form.Add("client_id", config.clientId)
+	req, err := http.NewRequest("POST", config.tokenEndpointURL, strings.NewReader(form.Encode()))
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	req.SetBasicAuth(config.clientId, config.clientSecret)
+
+	// Client
+	c := http.Client{}
+	res, err := c.Do(req)
+	if err != nil {
+		log.Print(err)
+		return
+	}
+
+	// Process Response
+	byteBody, err := ioutil.ReadAll(res.Body)
+	// close the reader when current method completes
+	defer res.Body.Close()
+
+	if err != nil {
+		log.Print(err)
+		return
+	}
+
+	accessTokenResponse := &model.AccessTokenResponse{}
+	json.Unmarshal(byteBody, accessTokenResponse)
+
+	appVar.AccessToken = accessTokenResponse.AccessToken
+	log.Printf("exchangeToken: token %s", appVar.AccessToken)
+
+	http.Redirect(writer, request, "http://localhost:8080", http.StatusFound)
+
+	log.Printf("exchangeToken: done")
 }
 
 func logout(writer http.ResponseWriter, request *http.Request) {
